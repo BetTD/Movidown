6bf9c4f689406bcd558c82241ddbf20780375c82
Raúl <between13131@gmail.com>
2025-03-02 18:03:23 +0100

    Add meta tags for embedding

--- front/index.php
+++ front/index.php
@@ -1,21 +1,32 @@
 <!doctype html>
 <html lang="es">
 <head>
-    <title>Front</title>
+    <title>Movidown</title>
+    <meta property='og:title'  content="Movidown">
+    <meta name='twitter:title' content="Movidown">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="¿Quieres saber si Movistar está bloqueando las IPs y webs de Cloudflare otra vez? ¡Esta es tu web!">
+    <meta property="og:image"  content="./img/movidown.png">
+    <meta name="twitter:image" content="./img/movidown.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="./fa/css/all.css">
+    <link rel="icon" type="image/png" href="./img/favicon.png">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="fetch.js"></script>
 </head>
 <body>
     <nav>
         <div class="navbar-section">
-            <p id="mini-status">¿Está Movistar bloqueando Cloudflare?</p>
+            <p id="mini-status">
+                ¿Está Movistar bloqueando Cloudflare?
+                <span class="mini-status loading">
+                    <i class="fa-solid fa-refresh fa-spin"></i>
+                </span>
+            </p>
         </div>
         <div class="navbar-section">
             <p class="darker"><i class="fa-solid fa-refresh fa-spin"></i> <span id="refresh-label">Esta página se recarga automáticamente</span></p>
